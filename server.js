5a73253de247ab0e68e2e3518b44563b2bb2bdb3
Josh Gulledge <jgulledge19@gmail.com>
2022-11-05 16:52:02 -0600

    Set up port, listener and first route

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`'Welcome to the Gitpub App!'`)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+});
